ede05cd0b7845e58b1cf8fd5bb4f603346e560e8
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-02 18:56:53 -0600

    part 2 day 2 soln added.

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,9 @@
+use day_2::alg::{day_2_p1_soln, day_2_p2_soln};
+
 fn main() {
-    println!("Hello, world!");
+    let safe_entries: i32 = day_2_p1_soln();
+    println!("safe entries: {}", safe_entries);
+
+    let damp_safe_entries = day_2_p2_soln();
+    println!("damp safe entries: {}", damp_safe_entries);
 }

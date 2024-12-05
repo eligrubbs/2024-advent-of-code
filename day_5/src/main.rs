55e77b632822ba8483f93b861328cf6998b1a16d
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-05 15:14:06 -0600

    day 5 code.

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,9 @@
+use day_5::alg::{day_5_p1_soln,day_5_p2_soln};
+
 fn main() {
-    println!("Hello, world!");
+    let sum_of_inorder: i32 = day_5_p1_soln();
+    println!("In-order middle sum: {}", sum_of_inorder);
+
+    let sum_of_outorder: i32 = day_5_p2_soln();
+    println!("Out-order middle snum: {}", sum_of_outorder);
 }

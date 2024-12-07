ea9e1fbead2d22b03c11738d099cb0506d76f734
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-07 11:36:33 -0600

    added code timer.

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,8 +1,15 @@
 use day_7::alg::{day_7_p1_soln, day_7_p2_soln};
+use std::time::{Instant, Duration};
+
 fn main(){
+    let now = Instant::now();
+
     let calib_score: i64 = day_7_p1_soln();
     println!("Calib score: {}", calib_score);
 
     let calib_score_3_operations: i64 = day_7_p2_soln();
     println!("Calib score 3 Operations: {}", calib_score_3_operations);
+
+    let elapsed: Duration = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }

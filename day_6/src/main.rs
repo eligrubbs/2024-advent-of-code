beec5dcb35b51fd1bd34d732c6c972ed9e4685b3
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-07 16:43:32 -0600

    time elapsed.

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,9 +1,15 @@
 use day_6::alg::{day_6_p1_soln, day_6_p2_soln};
+use std::time::{Instant, Duration};
 
 fn main() {
+    let now = Instant::now();
+
     let path_len: usize = day_6_p1_soln();
     println!("Path Len: {}", path_len);
 
     let square_count: usize = day_6_p2_soln();
-    println!("Squares possible {:?}", square_count)
+    println!("Squares possible {:?}", square_count);
+
+    let elapsed: Duration = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }

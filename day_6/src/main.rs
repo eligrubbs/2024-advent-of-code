cd3a62bab8ccc4b7524395965a2bb2f7e83dbfaa
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-07 15:57:34 -0600

    p1 done.

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let path_len: usize = day_6_p1_soln();
     println!("Path Len: {}", path_len);
 
-    let square_count = day_6_p2_soln();
+    let square_count: usize = day_6_p2_soln();
     println!("Squares possible {:?}", square_count)
 }

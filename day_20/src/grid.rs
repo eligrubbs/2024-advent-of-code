b448fc576e81c5e837d18576b1dc5c4df0acbcf1
Jakal02 <gitcontribs@eligrubbs.com>
2024-12-22 17:44:01 -0600

    djikstras to find path. still working on p1.

---
+++ day_20/src/grid.rs
@@ -0,0 +1,106 @@
+use std::{collections::{HashSet, HashMap}, fmt::{self, Debug}};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub map: HashMap<Coord, u32>,
+    pub typs: HashMap<Coord, char>,
+    pub start: Coord,
+    pub end: Coord,
+}
+
+impl Grid {
+    pub fn get_neighbors(&self, coord: &Coord) -> HashSet<(Coord, Direction)> {
+        let mut result: HashSet<(Coord, Direction)> = HashSet::new();
+        for dir in Direction::iter() {
+            let neighbor: Coord = coord.go(dir);
+            if *self.typs.get(&neighbor).unwrap() != '#' {
+                result.insert((neighbor, dir));
+            }
+        }
+        result
+    }
+}
+
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
+pub enum Direction {
+    N, E, S, W
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.str())
+    }
+}
+
+impl Direction {
+    /// Return direction as &str
+    pub fn str(&self) -> String {
+        use Direction::*;
+        (match self { N => "N", E => "E", S => "S", W => "W", }).to_string()
+    }
+
+    pub fn from(s: &str) -> Result<Direction, String> {
+        use Direction::*;
+        match s {
+            "N "=> Ok(N), "E" => Ok(E), "S" => Ok(S), "W" => Ok(W),
+            _ => Err(String::from(format!("Can't use {} to build a direction.", s))) 
+        }
+    }
+
+    pub fn turn_cw(&self) -> Direction {
+        use Direction::*;
+        match self {
+            N => E, E => S, S => W, W => N
+        }
+    }
+
+    pub fn turn_ccw(&self) -> Direction {
+        use Direction::*;
+        match self {
+            N => W, E => N, S => E, W => S
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+/// Assumes c is column based and that r is row based.
+/// Assumes coordinates in 4th quadrant and 4th only (plus axes)
+pub struct Coord {
+    pub r: usize,
+    pub c: usize,
+}
+
+impl fmt::Display for Coord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.str())
+    }
+}
+
+impl Coord {
+
+    /// returns coordinate as a string in `(row,col)` format.
+    pub fn str(&self) -> String {
+        format!("({},{})", self.r, self.c)
+    }
+
+    pub fn from(tup: (usize, usize)) -> Coord {
+        Coord{r: tup.0, c: tup.1}
+    }
+
+    /// Creates a new coordinate 1 step in the direction of `dir`.
+    /// This may be outside the 4th coordinate, so check bounds of the returned coordinate.
+    pub fn go(&self, dir: Direction) -> Coord {
+        use Direction::*;
+        match dir {
+            N => Coord::from((self.r-1, self.c)),
+            E => Coord::from((self.r, self.c+1)),
+            S => Coord::from((self.r+1, self.c)),
+            W => Coord::from((self.r, self.c-1)),
+        }
+    }
+}
--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,9 +1,10 @@
 use std::time::{Instant, Duration};
+use day_20::p1::day_20_p1_soln;
 
 fn main() {
     let now: Instant = Instant::now();
 
-    println!("Hello, world!");
+    day_20_p1_soln();
 
     let elapsed: Duration = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
